49e5603e28eef48734d9ca8d20b6e724c17047c7
Matthew <matthew_mcaleer@hotmail.com>
2022-02-27 15:52:37 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 function shout(string) {
-    return string.toUpperCase();
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-    return string.toLowerCase();
+  return string.toLowerCase();
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
 
 function logShout(string) {
-    console.log('HELLO');
-} 
-
-function logWhisper(string) {
-    console.log('hello');
-} 
-
+  console.log(string.toUpperCase());
+}
 
 function sayHiToHeadphonedRoommate(string) {
-    
-    if (string === string.toLowerCase()) {
-        return "I can't hear you!" 
-    }
-
-    if (string === string.toUpperCase()) {
-        return "YES INDEED!"
-    }
-
-    if (string === "Let's have dinner together!") {
-        return "I would love to!"
-    }
-
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else if (string = "Let's have dinner together!") {
+    return "I would love to!";
+  }
 }
